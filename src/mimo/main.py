3377068ddc8a277d7f2ce5b2658d4216ebaf25eb
Marius Arvinte <5852612+mariusarvinte@users.noreply.github.com>
2026-02-07 16:56:41 -0800

    Add PyTorch dependency and basic function (#2)
    * Add PyTorch dependency
    
    * Add matrix multiply code; equation 2 from paper
    
    * Add explicit numpy

--- src/mimo/main.py
+++ src/mimo/main.py
@@ -1,5 +1,16 @@
+import torch
+
+
 def main():
-    print("Hello from mimo!")
+    device = "cuda:0"
+    num_rx, num_tx, num_pilots = 64, 32, 16
+
+    H = torch.randn(num_rx, num_tx, dtype=torch.complex64, device=device)
+    P = torch.randn(num_tx, num_pilots, dtype=torch.complex64, device=device)
+    N = torch.randn(num_rx, num_pilots, dtype=torch.complex64, device=device)
+
+    Y = torch.matmul(H, P) + N
+    return Y
 
 
 if __name__ == "__main__":
