b7866e71aea3bb4edd5cf569a21a6b85662e61c0
Marius Arvinte <mariusarvinte@gmail.com>
2026-03-21 20:46:14 -0700

    Clean-up mimo.losses

--- src/mimo/losses.py
+++ src/mimo/losses.py
@@ -11,17 +11,6 @@ def add_noise_to_data(data: torch.Tensor, stddev: torch.Tensor) -> torch.Tensor:
 def score_training_loss(
     model_pred: torch.Tensor, noise: torch.Tensor, stddev: torch.Tensor
 ) -> torch.Tensor:
-    """
-    Computes a score-based generative model training loss
-
-    :param model_pred: The model prediction s(h + z)
-    :type model_pred: torch.complex
-    :shape model_pred: [B, 2, H, W]
-    :param noise: The added noise z
-    :type noise: torch.complex
-    :shape noise: [B, 2, H, W]
-    """
-
     loss = model_pred + noise / stddev.square()[..., None, None, None]
     loss = 1 / 2.0 * torch.sum(torch.square(loss), dim=(-1, -2, -3))
     weighted_loss = stddev.square() * loss
